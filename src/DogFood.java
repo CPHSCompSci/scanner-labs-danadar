48ce80e6068aff075bc621912a7a4fce87977d3f
Darren <Darren@10.19.21.146>
2019-10-18 12:16:00 -0500

    Foooood

--- src/DogFood.java
+++ src/DogFood.java
@@ -18,14 +18,54 @@ public class DogFood
    {
 		Scanner chopper = new Scanner(line);
 		double cups = 0;
-
 		//calc code goes here
-
+		String[] nums = line.split(" ");
+		for(String s: nums)
+		{
+			if(Integer.parseInt(s) >= 80)
+			{
+				cups += 7.5;
+			}
+			else if(Integer.parseInt(s) >= 60)
+			{
+				cups += 6;
+			}
+			else if(Integer.parseInt(s) >= 40)
+			{
+				cups += 4.5;
+			}
+			else if(Integer.parseInt(s) >= 20)
+			{
+				cups += 3.5;
+			}
+			else if(Integer.parseInt(s) >= 10)
+			{
+				cups += 2;
+			}
+			else if(Integer.parseInt(s) >= 8)
+			{
+				cups += 1.5;
+			}
+			else if(Integer.parseInt(s) >= 5)
+			{
+				cups += 1;
+			}
+			else if(Integer.parseInt(s) >= 2)
+			{
+				cups += 0.5;
+			}
+			else
+			{
+				System.out.println("An invalid number was entered");
+			}
+		}
+		this.amount = Math.ceil(cups / 10);
+		System.out.println(cups + "cups");
    }
 
    public double getAmount()
    {
-   	return 0;
+   		return amount;
    }
 
 	public String toString()
--- src/DogFoodRunner.java
+++ src/DogFoodRunner.java
@@ -10,5 +10,9 @@ public class DogFoodRunner
 {
 	public static void main(String[] args)
 	{
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the dog's weight on the first day of each week, separated by a space.");
+		DogFood food = new DogFood(input.nextLine());
+		System.out.println(food.getAmount() + "- 10 POUND BAGS");
 	}
 }
